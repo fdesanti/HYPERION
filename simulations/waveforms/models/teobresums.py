85c24763475b815e44c1486d73cfe0d25973c42f
fdesanti <f.desanti@studenti.unipi.it>
2024-04-28 00:47:59 +0200

    completed waveform generator and teobresums wrapper

--- simulations/waveforms/models/teobresums.py
+++ simulations/waveforms/models/teobresums.py
@@ -11,9 +11,6 @@ Instructions for the TEOBResumS waveform model:
 import torch
 import EOBRun_module
 
-from hyperion.core.distributions import q_uniform_in_components as q_prior
-
-
 
 def modes_to_k(modes):
     """
@@ -26,19 +23,23 @@ def modes_to_k(modes):
 class  TEOBResumSDALI():
     """Wrapper class for the TEOBResumS waveform model."""
     
-    def __init__(self, **kwargs):
+    def __init__(self, duration = 8, **kwargs):
         
         modes = [(2,1),(2,2),(3,3),(4,2)]
+
+        self.duration = duration
 
         self.default_kwargs = {
         # Initial conditions and output time grid
         'domain'             : 0,      # Time domain. EOBSPA is not available for eccentric waveforms!
-        'srate_interp'       : 2048,   # Srate at which to interpolate. Default = 4096.
+        'srate_interp'       : 2048.,   # Srate at which to interpolate. Default = 4096.
         'use_geometric_units': "no",   # output quantities in geometric units. Default = 1
         'interp_uniform_grid': "yes",  # interpolate mode by mode on a uniform grid. Default = "no" (no interpolation)
+        'initial_frequency'  : 10,     # in Hz if use_geometric_units = 0, else in geometric units
+        'ecc_freq'           : 1,      # Use periastron (0), average (1) or apastron (2) frequency for initial condition computation. Default = 1
 
         # Modes
-        'use_mode_lm'        : modes_to_k(modes),    # List of modes to use/output through EOBRunPy.
+        'use_mode_lm'        : modes_to_k(modes), # List of modes to use/output through EOBRunPy.
 
         # ode
         'ode_tmax'           : 20e4,
@@ -52,8 +53,8 @@ class  TEOBResumSDALI():
         'arg_out'            : "no",     # Output hlm/hflm. Default = "no"
         'output_hpc'         : "yes",    # Output waveform. Default = 1.
         }
-
-        self.kwargs = self.default_kwargs.update(kwargs)
+        
+        self.kwargs = self.default_kwargs.update(kwargs) if kwargs else self.default_kwargs
         return
         
     @property
@@ -64,45 +65,45 @@ class  TEOBResumSDALI():
     def has_torch(self):
         return False
     
+    
     @staticmethod
     def _check_compatability(parameters):
         """
         Check if the parameters are compatible with the model.
         """
-        if all(['m1', 'm2']) in parameters.keys():
+        if {'m1', 'm2'} <= parameters.keys():
             
-            m1, m2 = q_prior._sort_masses(parameters['m1'], 
-                                          parameters['m2'])
+            m2, m1 = sorted([parameters['m1'], parameters['m2']])
 
-            M = m1 + m2
-            q = m1 / m2
-
-            parameters['M'] = M
-            parameters['q'] = q
+            parameters['M'] = m1 + m2
+            parameters['q'] = m1 / m2
+        else:
+            raise ValueError('Parameters are not compatible with the model.')
 
         return parameters
     
 
-    def __call__(self, **waveform_parameters):
+    def __call__(self, waveform_parameters):
         """
         Compute the waveform.
         """
         # Update the parameters       
-        pars = self.kwargs
-        pars.update(waveform_parameters)
+        pars = self.kwargs.copy()
 
+        waveform_parameters = self._check_compatability(waveform_parameters)
+
+        pars.update(waveform_parameters)
+        
         # Run the model
-        eob_output = EOBRun_module.EOBRunPy(self.kwargs)
+        eob_output = EOBRun_module.EOBRunPy(pars)
         
         output = {}
-        if pars['output_hpc'] == 'yes':
-            output['hp'] = eob_output[0]
-            output['hc'] = eob_output[1]
         
-        elif pars['arg_out'] == 'yes':
-            output['t']  = torch.from_numpy(eob_output[0])
-            output['hp'] = torch.from_numpy(eob_output[1])
-            output['hc'] = torch.from_numpy(eob_output[2])
+        output['t']  = torch.from_numpy(eob_output[0])
+        output['hp'] = torch.from_numpy(eob_output[1])
+        output['hc'] = torch.from_numpy(eob_output[2])
+        
+        if pars['arg_out'] == 'yes':
             output['hlm'] = torch.from_numpy(eob_output[3])
             output['dyn'] = torch.from_numpy(eob_output[4])
         
--- simulations/waveforms/waveform_generator.py
+++ simulations/waveforms/waveform_generator.py
@@ -2,6 +2,7 @@ import torch
 import multiprocessing as mp
 
 from tqdm import tqdm
+from torch.nn.functional import pad
 from .models import EffectiveFlyByTemplate, TEOBResumSDALI
 
 models_dict = {'EffectiveFlyBy': EffectiveFlyByTemplate, 
@@ -23,12 +24,16 @@ class WaveformGenerator:
     """
     
     def __init__(self, 
-                 waveform_model:str, 
+                 waveform_model, 
+                 duration = 4,
+                 fs = 2048,
                  **waveform_model_kwargs):
 
         assert waveform_model in models_dict.keys(), f"Waveform model {waveform_model} not found. \
                                                        Available models are {models_dict.keys()}"
-
+        
+        self.duration = duration
+        self.fs = fs
         self.wvf_model = models_dict[waveform_model](**waveform_model_kwargs)
         
         
@@ -42,22 +47,117 @@ class WaveformGenerator:
     def has_torch(self):
         return self.wvf_model.has_torch
     
+    @property
+    def duration(self):
+        return self._duration
+    @duration.setter
+    def duration(self, value):
+        self._duration = value
+        
+    @property
+    def fs(self):
+        return self._fs
+    @fs.setter
+    def fs(self, value):
+        self._fs = value
 
 
-    def get_td_waveform(self, parameters, n_proc=None):
+    def _resize_waveform(self, t, hp, hc):
+        """
+        Resize the waveform to the desired duration.
+        """
+        N = int(self.duration * self.fs)
+        
+        #signal is longer --> crop
+        if hp.shape[-1] > N:
+            t = t[-N:]
+            hp = hp[-N:]
+            hc = hc[-N:]
+        
+        #signal is shorter --> symmetrically pad with zeros
+        elif hp.shape[-1] < N:
+            
+            pad_l = (N - hp.shape[-1]) // 2
+            pad_r =  N - hp.shape[-1] - pad_l
+
+            t  = pad(t,  (pad_l, pad_r))
+            hp = pad(hp, (pad_l, pad_r))
+            hc = pad(hc, (pad_l, pad_r))
+            
+        return t, hp, hc
+
+
+    def get_td_waveform(self, pars):
+        """
+        Computes the time domain waveform for a given set of parameters.
+
+        Args:
+        -----
+            pars: dict or TensorDict instance
+                Parameters of the waveform
+        
+        Returns:
+        --------
+             The output of the self.waveform_model __class__ method
+        """        
+        return self.wvf_model(pars)
+    
+
+    def _get_td_waveform_mp(self, i):
+
+        """
+        Compute the time domain waveform for a given set of parameters.
+        This function is intended to be used with multiprocessing.
+
+        Args:
+        -----
+            i: int
+                Index of the parameters to use. (Assigned by the multiprocessing pool)
+        
+        Returns:
+        --------
+             The output of the self.waveform_model __class__ method
+        """
+        
+        pars = self.parameters[i]
+        
+        return self.get_td_waveform(pars)
+    
+
+
+    def __call__(self, parameters, n_proc=None):
         
         # Check if the model is a torch model so that
         # it can handle batches of parameters and / or if parameters are batched
-        if self.has_torch or parameters.numel()<=1:
+        N = parameters.numel()
+        if self.has_torch or N<=1:
             return self.wvf_model(**parameters)
         
         # Otherwise, we exploit multiprocessing
         else:
             with mp.Pool(n_proc) as p:
-                results = list(tqdm(p.imap(self.wvf_model, parameters), total=len(parameters)))
-            
+                self.parameters = parameters
+                
+                hps = []
+                hcs = []
+                t_coals = []
+                
+                for results in tqdm(p.imap(self._get_td_waveform_mp, range(N)), total=N, ncols = 100, ascii=' ='):
+                    t = results['t']
+                    hp = results['hp']
+                    hc = results['hc']
 
+                    t, hp, hc = self._resize_waveform(t, hp, hc)
 
+                    t_coals.append(-t[len(t)//2])
+                    hps.append(hp)
+                    hcs.append(hc)
+                
+                hps = torch.stack(hps)
+                hcs = torch.stack(hcs)
+                t_coals = torch.tensor(t_coals).unsqueeze(-1)
+
+        return hps, hcs, t_coals
         
         
         
