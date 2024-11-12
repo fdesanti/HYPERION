28202dca0849ef5faa7b12d67cf4d3e97fdbd750
Federico De Santi <fdesanti@Computer-portatile-di-Federico.local>
2024-11-12 16:43:25 +0100

    PIP installation finally working

--- hyperion/core/neural_networks/__init__.py
+++ hyperion/core/neural_networks/__init__.py
@@ -1,4 +1,11 @@
-from .kenn_embedding import *
+try:
+    from .kenn_embedding import *
+except ImportError as e:
+    print(f"[ERROR]: Cannot import KennEmbedding: {e}")
+    print("[WARNING]: Please install the deepfastet package to use the KennEmbedding.")
+    KennEmbedding = None
+    pass
+
 from .strain_embedding import *
 from .strain_embedding_attention import *
 
