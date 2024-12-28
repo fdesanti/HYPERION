6b489e90af56cb3755ed4d427d5081f6db8ff171
Federico De Santi <f.desanti@campus.unimib.it>
2024-12-28 22:57:29 +0100

    added to_tensor as flatten alternative

--- hyperion/core/distributions/prior_distributions.py
+++ hyperion/core/distributions/prior_distributions.py
@@ -32,6 +32,10 @@ class TensorSamples(TensorDict):
         """Returns the samples as a single tensor"""
         return torch.stack([self[key] for key in self.keys()], dim=-1)
     
+    def to_tensor(self):
+        """Returns the samples as a single tensor"""
+        return self.flatten()
+    
     def numpy(self):
         """Returns the samples as a numpy array"""
         return self.flatten().cpu().numpy()
