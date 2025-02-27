0a38b2266910e0d936bc5b103bf0a2d2c79b1fcc
Federico De Santi <f.desanti@campus.unimib.it>
2025-02-27 15:19:36 +0100

    deprecated old likelihood [skip-ci]

--- hyperion/core/utilities/__init__.py
+++ hyperion/core/utilities/__init__.py
@@ -2,4 +2,3 @@ from .plot import latexify
 from .resample import resample
 from .interpolate import interp1d
 from .custom_logger import HYPERION_Logger
-
--- hyperion/training/__init__.py
+++ hyperion/training/__init__.py
@@ -1,6 +1,4 @@
-from .new_dataset_generator import DatasetGenerator
+from .dataset_generator import DatasetGenerator
 from ..simulations.asd import ASD_Sampler #so that we can import ASD_Sampler also from training
 from .trainer import Trainer
 from .train_utils import *
-
-
