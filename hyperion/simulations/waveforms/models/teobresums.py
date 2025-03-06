c3f1648d24689a2f56058c323eb94850f47e9d1f
Federico De Santi <f.desanti@campus.unimib.it>
2025-03-06 18:21:52 +0100

    teobresums dali docs ok

--- hyperion/simulations/waveforms/models/teobresums.py
+++ hyperion/simulations/waveforms/models/teobresums.py
@@ -36,11 +36,25 @@ class  TEOBResumSDALI():
     Wrapper class for the TEOBResumS waveform model.
     
     Args:
-    -----
         fs (float): Sampling frequency of the waveform.
         kwargs    : Additional keyword arguments to pass to the EOBRun_module generator.
+
+    By default, the waveform model is set to use the following parameters:
+        - **domain**             : 0        Time domain. EOBSPA is not available for eccentric waveforms!
+        - **use_geometric_units**: "no"     Output quantities in geometric units. Default = 1
+        - **interp_uniform_grid**: "yes"    Interpolate mode by mode on a uniform grid. Default = "no" (no interpolation)
+        - **initial_frequency**  : .02      in Hz if use_geometric_units = 0, else in geometric units
+        - **ecc_freq**           : 1        Use periastron (0), average (1) or apastron (2) frequency for initial condition computation
+        - **j_hyp**              : 4.0      Angular momentum for an hyperbolic capture
+        - **use_mode_lm**        : [(2,1),(2,2),(3,3),(4,2)]  List of modes to use/output through EOBRunPy.
+        - **ode_tmax**           : 20e4
+        - **ode_tstep_opt**      : "adaptive" Fixing uniform or adaptive
+        - **nqc**                : 'manual' options are ["none", "manual", "auto"]
+        - **nqc_coefs_flx**      : 'nrfit_spin202002' options are ["none", "nrfit_spin202002", "nrfit_nospin201602"]
+        - **nqc_coefs_hlm**      : 'nrfit_spin202002' options are ["none", "nrfit_spin202002", "nrfit_nospin201602", "compute"]
+        - **arg_out**            : "no",      Output hlm/hflm
+        - **output_hpc**         : "no",      Output waveform
     """
-    
     def __init__(self, fs, **kwargs):
         try:
             eob_module = import_module('EOBRun_module')
@@ -54,13 +68,13 @@ class  TEOBResumSDALI():
 
         self.default_kwargs = {
         # Initial conditions and output time grid
-        'domain'             : 0,      # Time domain. EOBSPA is not available for eccentric waveforms!
+        'domain'             : 0,           # Time domain. EOBSPA is not available for eccentric waveforms!
         'srate_interp'       : float(fs),   # Srate at which to interpolate. Default = 4096.
-        'use_geometric_units': "no",   # output quantities in geometric units. Default = 1
-        'interp_uniform_grid': "yes",  # interpolate mode by mode on a uniform grid. Default = "no" (no interpolation)
-        'initial_frequency'  : .02,     # in Hz if use_geometric_units = 0, else in geometric units
-        'ecc_freq'           : 1,      # Use periastron (0), average (1) or apastron (2) frequency for initial condition computation. Default = 1
-        'j_hyp'              : 4.0,    # Angular momentum for an hyperbolic capture. Default = 4.0
+        'use_geometric_units': "no",        # output quantities in geometric units. Default = 1
+        'interp_uniform_grid': "yes",       # interpolate mode by mode on a uniform grid. Default = "no" (no interpolation)
+        'initial_frequency'  : .02,         # in Hz if use_geometric_units = 0, else in geometric units
+        'ecc_freq'           : 1,           # Use periastron (0), average (1) or apastron (2) frequency for initial condition computation. Default = 1
+        'j_hyp'              : 4.0,         # Angular momentum for an hyperbolic capture. Default = 4.0
         
         # Modes
         'use_mode_lm'        : modes_to_k(modes), # List of modes to use/output through EOBRunPy.
@@ -70,9 +84,9 @@ class  TEOBResumSDALI():
         'ode_tstep_opt'      : "adaptive",        # Fixing uniform or adaptive. Default = 1 
         
         # nqcs
-        #'nqc'                : 'manual',           #options are ["none", "manual", "auto"]
-        #'nqc_coefs_flx'      : 'nrfit_spin202002', #options are ["none", "nrfit_spin202002", "nrfit_nospin201602"]
-        #'nqc_coefs_hlm'      : 'nrfit_spin202002', #options are ["none", "nrfit_spin202002", "nrfit_nospin201602", "compute"]
+        'nqc'                : 'manual',           #options are ["none", "manual", "auto"]
+        'nqc_coefs_flx'      : 'nrfit_spin202002', #options are ["none", "nrfit_spin202002", "nrfit_nospin201602"]
+        'nqc_coefs_hlm'      : 'nrfit_spin202002', #options are ["none", "nrfit_spin202002", "nrfit_nospin201602", "compute"]
 
         # Output parameters (Python)
         'arg_out'            : "no",     # Output hlm/hflm. Default = "no"
@@ -88,8 +102,6 @@ class  TEOBResumSDALI():
         for key, value in self.kwargs.items():
             print(f'{key}: {value}')
         print('\n')
-        #print(self.kwargs, '\n')
-        return
         
     @property
     def name(self):
@@ -127,7 +139,19 @@ class  TEOBResumSDALI():
 
     def __call__(self, waveform_parameters):
         """
-        Compute the waveform.
+        Computes the waveform.
+
+        Args:
+            waveform_parameters (TensorSamples): Dictionary containing the waveform parameters.
+
+        Returns:
+            dict: Dictionary containing the waveform data
+
+            -   **t** (torch.Tensor): Time array of the output waveform
+            -   **hp** (torch.Tensor): Plus polarization waveform
+            -   **hc** (torch.Tensor): Cross polarization waveform
+            -   **hlm** (torch.Tensor): List of modes hlm (only if ``arg_out`` is set to 'yes')
+            -   **dyn** (torch.Tensor): List of dynamics (only if ``arg_out`` is set to 'yes')
         """
         # Update the parameters      
         pars = self.kwargs.copy()
