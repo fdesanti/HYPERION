64d3824c663781039022224279f5848474b86cb4
Federico De Santi <f.desanti@campus.unimib.it>
2025-03-07 17:47:58 +0100

    added source code link in sphinx

--- docs/conf.py
+++ docs/conf.py
@@ -15,7 +15,7 @@ release = '1.0.0'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.napoleon', 'sphinx.ext.mathjax', "myst_parser"]
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.napoleon', 'sphinx.ext.mathjax', 'sphinx.ext.viewcode', "myst_parser"]
 
 templates_path = ['_templates']
 
