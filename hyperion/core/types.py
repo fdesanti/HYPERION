5830e408e4f7f4e4619f3caf748e33cbda64fae9
Federico De Santi <f.desanti@campus.unimib.it>
2025-09-22 10:04:45 +0200

    added TensorSamples features

--- hyperion/core/types.py
+++ hyperion/core/types.py
@@ -8,13 +8,21 @@ class TensorSamples(TensorDict):
     """Wrapper class for TensorDict to better manage samples"""
     
     def flatten(self):
-        """Returns the samples as a single tensor"""
-        return torch.stack([self[key] for key in self.keys()], dim=-1)
+        """Returns the samples as a single flattened tensor"""
+        return self.to_tensor().flatten()
     
     def to_tensor(self):
         """Returns the samples as a single tensor"""
-        return self.flatten()
+        return torch.stack([self[key] for key in self.keys()], dim=-1)
+
+    def tensor(self):
+        """Returns the samples as a single tensor"""
+        return self.to_tensor()
     
     def numpy(self):
         """Returns the samples as a numpy array"""
         return self.flatten().cpu().numpy()
+
+    def numpy_dict(self):
+        """Returns the samples as a dict of numpy arrays"""
+        return {key: self[key].cpu().numpy() for key in self.keys()}
